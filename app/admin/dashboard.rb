2efe37c162d6faabb6fca19c6a59abf575d9124e
brimatteng <btenggren20@gmail.com>
2014-08-27 10:38:34 -0400

    Setup dashboard to show all objects info

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,36 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+
+    columns do
+        column do
+            panel "Articles" do
+                ul do
+                    Article.all.map do |a|
+                        li link_to(a.title, admin_article_path(a))
+                    end
+                end
+            end
+        end
+        column do
+            panel "Artists" do
+                ul do
+                    Artist.all.map do |a|
+                        li link_to(a.name, admin_artist_path(a))
+                    end
+                end
+            end
+        end
+        column do
+            panel "Items" do
+                ol do
+                    Item.all.map do |i|
+                        li link_to(i.title, admin_item_path(i))
+                    end
+                end
+            end
+        end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
