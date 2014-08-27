a184c975fac0f1803cbab0230649f5315c447807
nnovak13 <nnovak@fordham.edu>
2014-08-27 14:43:24 -0400

    Add image uploading to artist admin form

--- app/admin/artist.rb
+++ app/admin/artist.rb
@@ -13,5 +13,16 @@ ActiveAdmin.register Artist do
   permit_params :name, :bio, :contact_email, :img_thumb, :img_main
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  form do |f|
+    f.inputs "Item Details" do
+      f.input :name
+      f.input :bio
+      f.input :contact_email
 
+      f.input :img_main, :required =>false, :as => :file
+      f.input :img_thumb, :required =>false, :as => :file
+      # Will preview the image when the object is edited
+    end
+    f.actions
+  end
 end
