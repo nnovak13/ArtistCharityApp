331e2eff79073dfe76ef291649d008667b8b2f85
brimatteng <btenggren20@gmail.com>
2014-08-28 17:55:28 -0400

    add show view for artists and styles!

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,8 @@ class ArtistsController <ApplicationController
   def index
     @artists = Artist.all
   end
+  def show
+    @artist = Artist.find(params[:id])
+    @items = Item.where(artist_id: @artist)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only:[:index]
   resources :articles, only:[:index]
-  resources :artists, only:[:index]
+  resources :artists, only:[:index, :show]
   resources :items, only:[:index]
 
   root 'home#show'
